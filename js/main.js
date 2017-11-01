e6f78e4d9a1eb9b2be6324659f449642170e3634
Nick Sladic <nick.sladic@g.uky.edu>
2017-11-01 13:12:27 -0400

    added jquery isotope filtering functionality

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  console.log("DOM LOADED");
+  $('#gallery').imagesLoaded(function(){
+    initGallery();
+  });
+  function initGallery(){
+  	// isotope
+  	var $alumni = $('.alumnis');
+  	console.log(alumni);
+
+  	$alumni.isotope({
+  	  itemSelector : '.bootcampAlumni',
+  	  layoutMode : 'fitRows'
+  	});
+
+  	$('#filters a').on("click", function(){
+  	  var selector = $(this).attr('data-filter');
+  		console.log(selector);
+  	  $alumni.isotope({ filter: selector });
+  	  return false;
+  	});
+
+  	$('select.filter').change(function(){
+  		$("select option:selected").each(function () {
+  			var selector = $(this).attr('data-filter');
+
+  			$alumni.isotope({ filter: selector });
+  			return false;
+  		});
+  	});
+  }
+
+});
